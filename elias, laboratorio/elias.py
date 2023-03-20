10227fe84478bdd3d1df6a27cb1393404fa8d456
elias <jg057440@gmail.com>
2023-03-20 11:52:35 -0600

    Texto random

---
+++ elias, laboratorio/elias.py
@@ -0,0 +1,13 @@
+edad=17
+print (edad) 
+
+nombre (elias garcia)
+print (nombre) 
+pi= 3.1416
+print= (pi) _
+van_a_ganar_el_ano= "false" 
+print (van_a_ganar_el_ano)
+print(type(nombre)) 
+
+print ()
+print (van_a_ganar_el_ano)
