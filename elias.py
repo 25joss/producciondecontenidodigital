9b2ae85489a98585baf33ef013234e7a8c4a8887
elias <jg057440@gmail.com>
2023-03-22 08:38:29 -0600

    se agrego una linea

---
+++ elias.py
@@ -0,0 +1,42 @@
+edad=17
+print (edad) 
+
+print (type(edad))
+
+nombre="elias garcia"
+print (nombre) 
+
+print (type(nombre))
+
+pi=3.1416
+print (pi)
+
+print (type(pi)) 
+vamos_a_ganar_el_examen=False
+
+print (vamos_a_ganar_el_examen)
+
+print(type(vamos_a_ganar_el_examen))
+
+diasdls=7
+
+print(diasdls)
+
+print(f"mi nombre es {nombre}")
+
+#"estra variableme enseña a unir dos variable"
+'''nos vamos a ver al final del año
+ya graduados'''
+
+num1="30"
+num2="15"
+result= num1 + num2
+
+print (result)
+result2= result  num1
+print (result2)
+resul3= result2 * 2
+print (result)
+
+modulo= 250 %3
+print(modulo) 
